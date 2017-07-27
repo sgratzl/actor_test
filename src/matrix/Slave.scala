662299df57db448a2a121a5f192a5c59e4ff5bfb
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-07-27 07:00:28 +0200

    optimize memory

--- src/matrix/Slave.scala
+++ src/matrix/Slave.scala
@@ -6,9 +6,9 @@ import scala.actors.remote._
 import scala.actors.remote.RemoteActor._
 import scala.collection.mutable
 
-case class Init(n: Int)
+case class Init(n: Short)
 
-case class At(i: Int, j: Int)
+case class At(i: Short, j: Short)
 
 case class Assign(at: At, a: Int, b: Int)
 case class Assigns(v: Iterable[Assign])
@@ -32,7 +32,7 @@ case class SlaveLookup(n: Int, slaves: Int, index: Int, self: Actor) {
   }
 
   def nodesOf(slave: Int): Map[At, GridNode] = {
-    val pairs = for (i <- 0 until n; j <- 0 until n if ((i * n) + j) % slaves == slave) yield At(i, j)
+    val pairs = for (i <- 0 until n; j <- 0 until n if ((i * n) + j) % slaves == slave) yield At(i.asInstanceOf[Short], j.asInstanceOf[Short])
     pairs.map(_ -> new GridNode()).toMap
   }
 }
@@ -43,10 +43,11 @@ class Master(val slaves: Int, val a: Matrix, val b: Matrix, val result: (Matrix)
     val n = a.nrow
     val slaveAt = SlaveLookup(n, slaves, -1, null)
 
-    slaveAt.remotes.foreach(_ ! Init(n))
+    slaveAt.remotes.foreach(_ ! Init(n.asInstanceOf[Short]))
 
     val assignments = for (i <- 0 until n; j <- 0 until n) yield {
-      (slaveAt(At(i, j)), Assign(At(i, j), a.values(i)(j), b.values(i)(j)))
+      val at = At(i.asInstanceOf[Short], j.asInstanceOf[Short])
+      (slaveAt(at), Assign(at, a.values(i)(j), b.values(i)(j)))
     }
     assignments.groupBy((kv) => kv._1).foreach((kvs) => kvs._1 ! Assigns(kvs._2.map(_._2)))
 
@@ -60,6 +61,7 @@ class Master(val slaves: Int, val a: Matrix, val b: Matrix, val result: (Matrix)
           c(i)(j) = v
           missing -= 1
         case Collects(vs) =>
+          println("got collect")
           for ((Collect(At(i,j), v)) <- vs) {
             c(i)(j) = v
           }
@@ -84,6 +86,26 @@ class Slave(val index: Int, val slaves: Int) extends Actor {
         val master = sender
         val slaveAt = SlaveLookup(n, slaves, index, this)
         val nodes = slaveAt.nodesOf(index)
+
+        def sendAll(toSend: Iterable[(OutputChannel[Any], SetValue)]): Int = {
+          var dec = nodes.size * 2
+          toSend.groupBy((kv) => kv._1).filter((kvs) => {
+            if (kvs._1 != this) {
+              true
+            } else {
+              //apply directly
+              for ((_, SetValue(at, s, v)) <- kvs._2) {
+                //println(at, "set")
+                val node = nodes(at)
+                if (s == 'a) node.a = v else node.b = v
+              }
+              dec -= kvs._2.size
+              false
+            }
+          }).foreach((kvs) => kvs._1 ! SetValues(kvs._2.map(_._2)))
+
+          dec
+        }
         //println("handle: ", nodes.keys)
         var dec = nodes.size
         loopWhile(dec > 0) {
@@ -95,6 +117,7 @@ class Slave(val index: Int, val slaves: Int) extends Actor {
               node.b = b
               dec -= 1
             case Assigns(vs) =>
+              println("got assigns")
               for(Assign(at, a, b) <- vs) {
                 val node = nodes(at)
                 node.a = a
@@ -104,15 +127,14 @@ class Slave(val index: Int, val slaves: Int) extends Actor {
           }
         } andThen {
           val toSend = (for (Tuple2(at, node) <- nodes) yield {
-            val targetA = At(at.i, (at.j - at.i + n) % n)
+            val targetA = At(at.i, ((at.j - at.i + n) % n).asInstanceOf[Short])
             val a = (slaveAt(targetA), SetValue(targetA, 'a, node.a))
-            val targetB = At((at.i - at.j + n) % n, at.j)
+            val targetB = At(((at.i - at.j + n) % n).asInstanceOf[Short], at.j)
             val b = (slaveAt(targetB), SetValue(targetB, 'b, node.b))
             Array[(OutputChannel[Any], SetValue)](a, b)
           }).flatten
-          toSend.groupBy((kv) => kv._1).foreach((kvs) => kvs._1 ! SetValues(kvs._2.map(_._2)))
+          dec = sendAll(toSend)
 
-          dec = nodes.size * 2
           loopWhile(dec > 0) {
             react {
               case SetValue(at, s, v) =>
@@ -121,6 +143,7 @@ class Slave(val index: Int, val slaves: Int) extends Actor {
                 if (s == 'a) node.a = v else node.b = v
                 dec -= 1
               case SetValues(vs) =>
+                println("got set values")
                 for(SetValue(at, s, v) <- vs) {
                   //println(at, "set")
                   val node = nodes(at)
@@ -135,15 +158,14 @@ class Slave(val index: Int, val slaves: Int) extends Actor {
               nodes.values.foreach((v) => v.c += v.a * v.b)
 
               val toSend = (for (Tuple2(at, node) <- nodes) yield {
-                val targetA = At(at.i, (at.j - 1 + n) % n)
+                val targetA = At(at.i, ((at.j - 1 + n) % n).asInstanceOf[Short])
                 val a = (slaveAt(targetA), SetValue(targetA, 'a, node.a))
-                val targetB = At((at.i - 1 + n) % n, at.j)
+                val targetB = At(((at.i - 1 + n) % n).asInstanceOf[Short], at.j)
                 val b = (slaveAt(targetB), SetValue(targetB, 'b, node.b))
                 Array[(OutputChannel[Any], SetValue)](a, b)
               }).flatten
-              toSend.groupBy((kv) => kv._1).foreach((kvs) => kvs._1 ! SetValues(kvs._2.map(_._2)))
+              dec = sendAll(toSend)
 
-              dec = nodes.size * 2
               loopWhile(dec > 0) {
                 react {
                   case SetValue(at, s, v) =>
@@ -152,6 +174,7 @@ class Slave(val index: Int, val slaves: Int) extends Actor {
                     if (s == 'a) node.a = v else node.b = v
                     dec -= 1
                   case SetValues(vs) =>
+                    println("got set values")
                     for(SetValue(at, s, v) <- vs) {
                       //println(at, "set")
                       val node = nodes(at)
